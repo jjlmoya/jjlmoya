1d39eb4b5854959900390196530e724c2306a5f3
jjlmoya <jj.lopez@avoristravel.com>
2020-04-11 23:25:14 +0200

    Add Navicons component

---
+++ components/Navicon/Default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="navicon">
+        <img class="navicon__image" :src="image" :alt="title">
+    </div>
+</template>
+<style lang="scss">@import 'default.scss';
+</style>
+
+<script>
+    export default {
+        props: {
+            image: {
+                type: String,
+                default: ''
+            },
+            title: {
+                type: String,
+                default: ''
+            }
+        }
+    }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
     <div class="p-home" :class="theme">
         <Layout>
             <Grid>
-                <h1>Portfolio</h1>
-                <h2>Habilidades</h2>
-                <h2>Proyectos</h2>
-                <h2>Empresas</h2>
+                <Navicon :image="'https://img.pngio.com/animation-model-sheet-cartoon-idea-business-worker-png-download-animated-business-png-1680_1680.png'" :title="'About'" />
+                <Navicon :image="'https://www.itsyourskills.com/wp-content/uploads/2019/12/multiple-skills.png'" :title="'Habilidades'" />
+                <Navicon :image="'https://images.squarespace-cdn.com/content/v1/553490f8e4b0e276c297620e/1529797469880-PC5WAJE1KZ2EO0ID5511/ke17ZwdGBToddI8pDm48kB-2zlWSwJppa90cCEGAmqhZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIF5UtvEzrMfxdVO7ybNbRoDexafUbiSiqrQWjG4iGukE/ideatodone+banner_v07_B.png'" :title="'Habilidades'" />
+                <Navicon :image="'https://foryoubg.org/wp-content/uploads/2019/01/free-time.png'" :title="'Habilidades'" />
             </Grid>
         </Layout>
     </div>
@@ -18,6 +18,7 @@
 <script>
     import Layout from '@/layouts/Default.vue'
     import Grid from '@/layouts/Grid.vue'
+    import Navicon from '@/components/Navicon/Default.vue'
 
     import SeoService from '@/services/seoService'
     const SEO_PAGE_DATA = new SeoService({
@@ -29,7 +30,8 @@
         name: 'Home',
         components: {
             Layout,
-            Grid
+            Grid,
+            Navicon
         },
         head () {
             return SEO_PAGE_DATA.getMetas()
