22fb5bb71b1434dbd778cef4f9892ae4969c83ba
jjlmoya <jj.lopez@avoristravel.com>
2020-04-15 22:17:54 +0200

    Add menu

--- components/Menu/Default.vue
+++ components/Menu/Default.vue
@@ -4,17 +4,41 @@
         <div class="menu__element">
             JJLMOYA
         </div>
-        <div class="menu__element">
-            <a href=""><img src="" alt="linkedin"></a>
-            <a href=""><img src="" alt="github"></a>
+        <div class="menu__element menu__icons">
+            <a class="menu__element-icon" href="https://www.linkedin.com/in/jjlmoya/">
+                <img src="https://lh3.googleusercontent.com/proxy/nmBUcdg1NFKqTSia0TKUDkuB5mcmjlbuLXyEfzyMCbhNwqiFGl4_okFywnkLIwVUPA3D-hH2ZbSJtbxZXbN21AB9rr66O5vsKTbk1zCvg2u8dp3XchYTLyyn0dACQfU" alt="linkedin">
+            </a>
+            <a class="menu__element-icon" href="https://github.com/jjlmoya">
+                <img src="https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/15221874341530102002-256.png" alt="github">
+            </a>
         </div>
-        <div class="menu__element">
-            Proyectos
-        </div>
-        <div class="menu__element">
-            Empresas
+        <div v-for="nav in navigation" :key="nav" class="menu__element text">
+            <a :href="link"> {{ nav.name }} </a>
         </div>
     </nav>
 </template>
 <style lang="scss">@import 'default.scss';
 </style>
+
+<script>
+    export default {
+        name: 'MenuNavigation',
+        components: {},
+        computed: {
+            navigation () {
+                return [{
+                            name: 'Projects',
+                            link: ''
+                        },
+                        {
+                            name: 'Tools',
+                            link: ''
+                        },
+                        {
+                            name: 'Companies',
+                            link: ''
+                        }]
+            }
+        }
+    }
+</script>
