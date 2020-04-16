ccc40867ae95f32aa3ca807e4797c9b98f341045
jjlmoya <jj.lopez@avoristravel.com>
2020-04-16 18:01:15 +0200

    Array per social media

--- components/Menu/Default.vue
+++ components/Menu/Default.vue
@@ -5,11 +5,8 @@
             JJLMOYA
         </div>
         <div class="menu__element menu__icons">
-            <a class="menu__element-icon" href="https://www.linkedin.com/in/jjlmoya/">
-                <img src="https://lh3.googleusercontent.com/proxy/nmBUcdg1NFKqTSia0TKUDkuB5mcmjlbuLXyEfzyMCbhNwqiFGl4_okFywnkLIwVUPA3D-hH2ZbSJtbxZXbN21AB9rr66O5vsKTbk1zCvg2u8dp3XchYTLyyn0dACQfU" alt="linkedin">
-            </a>
-            <a class="menu__element-icon" href="https://github.com/jjlmoya">
-                <img src="https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/15221874341530102002-256.png" alt="github">
+            <a v-for="social in socialMedias" :key="social" class="menu__element-icon" :href="social.link">
+                <img :src="social.image" :alt="social.name">
             </a>
         </div>
         <div v-for="nav in navigation" :key="nav" class="menu__element text">
@@ -25,6 +22,20 @@
         name: 'MenuNavigation',
         components: {},
         computed: {
+            socialMedias () {
+                return [
+                    {
+                        name: 'Linkedin',
+                        image: 'https://lh3.googleusercontent.com/proxy/Lcc45ZRsSN3L8xFkXnp1rE3X34oX2KV7fEiA7GDiQXGzqoaJqKM4bOIh0JezI6V5UIhe_MRuExUHQBJ6GLv01GXibxkEVFdcWsijlMxsUgxaxuPOAQ',
+                        link: 'https://www.linkedin.com/in/jjlmoya/'
+                    },
+                    {
+                        name: 'Github',
+                        image: 'https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/15221874341530102002-256.png',
+                        link: 'https://github.com/jjlmoya'
+                    }
+                ]
+            },
             navigation () {
                 return [{
                             name: 'Projects',
