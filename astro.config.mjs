f791bd11c4266c65562e6160f69fe3796f8b92d7
jjlmoya <juanjo1989@gmail.com>
2025-11-21 08:41:40 +0100

    feat: add open graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jjlmoya.com',
+  site: 'https://jjlmoya.es',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
