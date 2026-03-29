519b013dbd3000684437d3ec9c0d7b975d2a3ca8
jjlmoya <juanjo1989@gmail.com>
2026-03-29 20:42:39 +0200

    feat: add tideCalculator and underkneel calculator

--- src/data/utilities/nautical.ts
+++ src/data/utilities/nautical.ts
@@ -5,6 +5,23 @@ export const nauticalSection: SectionData = {
     slug: "vela-y-nautica",
     icon: "mdi:sailing",
     theme: "blue",
-    utilities: [],
+    utilities: [
+        {
+            href: "/utilidades/calculadora-altura-marea/",
+            iconBg: "mdi:waves",
+            iconFg: "mdi:sailing",
+            title: "Calculadora de Altura de Marea",
+            description: "Calcula la altura del agua en cualquier momento usando la Regla de los Dozavos.",
+            color: "#0074B7",
+        },
+        {
+            href: "/utilidades/calculador-paso-de-bajo/",
+            iconBg: "mdi:anchor",
+            iconFg: "mdi:ship-wheel",
+            title: "Calculador de Paso de Bajo",
+            description: "Determina si tienes agua suficiente para pasar por un punto crítico según tu calado.",
+            color: "#003B73",
+        }
+    ],
 };
 
