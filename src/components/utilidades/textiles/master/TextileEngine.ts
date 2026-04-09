4352e7ca089012acfb7b5a26a1de2e4bddd3ce4e
jjlmoya <juanjo1989@gmail.com>
2026-04-09 20:53:21 +0200

    fix: missing category

--- src/components/utilidades/textiles/master/TextileEngine.ts
+++
@@ -1,5 +0,0 @@
-import { TEXTILE_DATA, type TextileFiber } from "../../../../data/utilities/textiles";
-
-export type CareInstructions = TextileFiber;
-
-export const TEXTILE_CARE_DATA = TEXTILE_DATA;
