408277f9ee29a8389f98f1e690ddce4b4a644e45
jjlmoya <juanjo1989@gmail.com>
2026-04-09 21:39:16 +0200

    feat: add sitemap alternates

--- src/pages/sitemap-hreflang.xml.ts
+++ src/pages/sitemap-hreflang.xml.ts
@@ -54,13 +54,7 @@ export const GET: APIRoute = async () => {
 
     entries.push(urlEntry(`${SITE}/utilidades/`, getUtilitiesHubAlternates()));
 
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-  xmlns:xhtml="http://www.w3.org/1999/xhtml"
->
-${entries.join("\n")}
-</urlset>`;
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n${entries.join("\n")}\n</urlset>`;
 
     return new Response(xml, {
         headers: { "Content-Type": "application/xml; charset=utf-8" },
